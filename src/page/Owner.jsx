dbac89802aa416c71f479d23dfe8e3cf02e61477
pukpuk555 <jester2540@gmail.com>
2024-06-10 11:12:15 +0700

    fix picture in owner page

--- src/page/Owner.jsx
+++ src/page/Owner.jsx
@@ -6,7 +6,7 @@ function Owner() {
       <div className="flex flex-col items-center">
         <h2 className="font-bold text-4xl mt-10">Noopuk-Group G-09</h2>
         <img
-          src="src\assets\noopuk.jpg"
+          src="/noopuk.jpg"
           alt="Noopuk"
           className="w-[300px] h-[300px] m-4"
         />
